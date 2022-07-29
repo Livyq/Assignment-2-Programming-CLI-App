609d708237cac16ec992d783c1a0afc350ae22da
Olivia Qiu <olivia.qiu@me.com>
2022-07-29 19:07:29 +1000

    add AppMusoPlan.js

---
+++ AppMusoPlan.js
@@ -0,0 +1,47 @@
+// get input from the user.
+const ps = require('prompt-sync');//load prompt-sync module
+const prompt = ps(); // call ps function 
+const musoFunctions=require('./musoFunction.js')
+
+
+
+// user choose from eight options 
+while (true){
+    if (choice == 0){
+        //show input to users
+        console.log('======================================');
+    }
+    // option 1 create a musician
+    else if(choice == 1){ 
+        choice = 0;
+    }
+    else if(choice == 2){
+
+    }
+    else if(choice == 3){
+
+    }
+    else if(choice == 4){
+
+    }
+    else if(choice == 5){
+
+    }
+    else if(choice == 6){
+
+    }
+    else if(choice == 7){
+
+    }
+    else if(choice == 8){
+
+    }
+    else if(choice == 9){
+
+    }
+    else{
+
+    }
+
+
+}
