39c7fa97b4aaa76ec784ebabe82c66ded56c9fab
Olivia Qiu <olivia.qiu@me.com>
2022-07-29 18:59:59 +1000

    add troupe class

---
+++ Troupe.js
@@ -0,0 +1,17 @@
+class Troupe {
+  constructor(name, minimumDuration, genre) {
+    this.name = name;
+    this.minimumDuration = minimumDuration;
+    this.genre = genre;
+    this.musicians = [];
+  }
+  addMusician(musician) {
+    this.musicians.push(musician);
+  }
+  summary() {
+    return `${this.name} is a ${this.genre} troupe that has ${this.musicians.length} musicians.`;
+  }
+  details() {
+    return `${this.name} is a ${this.genre} troupe that has ${this.musicians.length} musicians.`;
+  }
+}
