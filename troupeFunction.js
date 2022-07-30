8ddc08a797772583e7bb227b317672e96d81036f
Olivia Qiu <olivia.qiu@me.com>
2022-07-30 21:05:44 +1000

    create troupeFunction.js

---
+++ troupeFunction.js
@@ -0,0 +1,6 @@
+// var troupeList = new Array();
+// let troupeName = inputFunction.stringInput("Please put in your name(3-30 characters)",3,30); 
+// let minimumDuration = inputFunction.numberInput("Please put in hours(between 0.5 and 3 hours) ", {type: 'integer', min: 51})); 
+// let troupeGenre = inputFunction.listInput("Please select one genre", genreList); 
+// const newTroupe =  new Troupe(troupeName,minimumDuration,troupeGenre);
+// troupeList.push(newTroupe);
